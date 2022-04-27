4d464309f9c7e7d8f9ae18650afd64c4843668db
MarlinRLee <Marlinrlee0@gmail.com>
2022-04-27 13:45:29 -0500

    set to work for madison

--- lib/DataPathName.R
+++ lib/DataPathName.R
@@ -11,21 +11,9 @@ HFGCasePath = function(BaseDir){
 LIMSWastePath = function(BaseDir){
   PathName = paste0(BaseDir,"data/processed/DHSWasteData-4_21_2022.csv")
   return(PathName)
-}#LIMSWasteData_02-09-22.csv
+}#LIMSWasteData_02-09-22.csv  
 
 LIMSCasePath = function(BaseDir){
   PathName = paste0(BaseDir,"data/processed/DHSCaseData-4_21_2022.csv")
   return(PathName)
 }#MMSD_Interceptor_Cases_2_7_22.csv
-
-RmdKnitPar = function(input, ...){
-  
-  rmarkdown::render(
-    input,
-    output_file = paste0("RmDOutput/",
-                         xfun::sans_ext(input), '-', Sys.Date(), '.',
-                         xfun::file_ext(input)
-    ),
-    envir = globalenv()
-  )
-}
