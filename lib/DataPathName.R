ecc77564a020c06328f4032b19b05eecc06f929d
MarlinRLee <Marlinrlee0@gmail.com>
2022-04-26 22:47:23 -0500

    Interceptor, mainstory set up with local data

--- lib/DataPathName.R
+++ lib/DataPathName.R
@@ -1,20 +1,20 @@
 HFGWastePath = function(BaseDir){
-  PathName = paste0(BaseDir,"COVID-19_WastewaterAnalysis/data/processed/HFGWasteData011422.csv")
+  PathName = paste0(BaseDir,"data/processed/HFGWasteData011422.csv")
   return(PathName)
 }
 
 HFGCasePath = function(BaseDir){
-  PathName = paste0(BaseDir,"COVID-19_WastewaterAnalysis/data/processed/HFGCaseData_2021-05-07.csv")
+  PathName = paste0(BaseDir,"data/processed/HFGCaseData_2021-05-07.csv")
   return(PathName)
 }
 
 LIMSWastePath = function(BaseDir){
-  PathName = paste0(BaseDir,"COVID-19_WastewaterAnalysis/data/processed/LIMSWasteData_02-09-22.csv")
+  PathName = paste0(BaseDir,"data/processed/LIMSWasteData_02-09-22.csv")
   return(PathName)
 }
 
 LIMSCasePath = function(BaseDir){
-  PathName = paste0(BaseDir,"COVID-19_WastewaterAnalysis/data/processed/MMSD_Interceptor_Cases_2_7_22.csv")
+  PathName = paste0(BaseDir,"data/processed/MMSD_Interceptor_Cases_2_7_22.csv")
   return(PathName)
 }
 
