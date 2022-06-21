3031ef51667db789643879b6b051df268313a90a
Marlin Lee <Marlinrlee0@gmail.com>
2022-06-21 13:10:01 -0500

    add hash based tests

--- DSIWasteWater/DSIwastewaterscrpit.R
+++ DSIWasteWater/DSIwastewaterscrpit.R
@@ -6,6 +6,6 @@ dir.create("inst/doc", recursive = TRUE)
 file.copy(dir("doc", full.names=TRUE), "inst/doc", overwrite=TRUE)
 build(vignettes = FALSE)
 install(quick=FALSE)
-
+devtools::test()#VerySlow
 check(vignettes  = FALSE)
 vignette(package = "DSIWasteWater")
---
+++ DSIWasteWater/tests/testthat/test-Vignette1.R
@@ -0,0 +1,20 @@
+test_that("Test vignettes DHSTopLevelAnalysis_Base.Rmd produces expected output", {
+  DF <- data(Data_wastewater, package = "DSIWasteWater")
+  
+  expect_identical(digest::digest(DF),"10d1158b3becd28689f932ce5018f013")
+  
+  workset4 <- BuildWorkSheet4(Data_wastewater)
+  
+  workset4 <- workset4%>% 
+    filter(n >= 150)
+  
+  expect_identical(digest::digest(workset4),"3c7d779a66b8c81a3d512b54e1c15279")
+  
+  reg_estimates <- BuildRegressionEstimateTable(workset4)
+  
+  expect_identical(digest::digest(reg_estimates),"e0922bb0966e1fa7265d77333e4ca69d")
+  
+  DHSPlot <- DHSTopLevelPlots(reg_estimates, workset4)
+  
+  expect_identical(digest::digest(DHSPlot),"14550b35a259948bbccafae0c5187df6")
+})
