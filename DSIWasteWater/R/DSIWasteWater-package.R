d14e252aa387e16b5e74a25a0bba82fd5692d56b
Marlin Lee <Marlinrlee0@gmail.com>
2022-06-22 16:28:01 -0500

    delete .lintr file and add import

--- DSIWasteWater/R/DSIWasteWater-package.R
+++ DSIWasteWater/R/DSIWasteWater-package.R
@@ -1,4 +1,4 @@
-#' @importFrom stats ccf dgamma lm na.omit na.pass quantile sd weighted.mean
+#' @importFrom lag stats ccf dgamma lm na.omit na.pass quantile sd weighted.mean
 #' @importFrom dplyr ungroup group_split n arrange desc %>% bind_rows group_by summarise sym summarise select mutate rename pull filter
 #' @importFrom tidyr pivot_wider pivot_longer all_of
 #' @importFrom ggplot2 aes theme element_text scale_x_date scale_fill_manual scale_fill_gradient element_blank facet_grid geom_line geom_point geom_rect geom_tile ggplot
