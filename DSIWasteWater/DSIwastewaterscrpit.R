ae47dcf3cc63a513ba8d4d4707cf8d844e5ceae3
Marlin Lee <Marlinrlee0@gmail.com>
2022-06-22 16:11:50 -0500

    add plot testing that works

--- DSIWasteWater/DSIwastewaterscrpit.R
+++ DSIWasteWater/DSIwastewaterscrpit.R
@@ -6,6 +6,5 @@ dir.create("inst/doc", recursive = TRUE)
 file.copy(dir("doc", full.names=TRUE), "inst/doc", overwrite=TRUE)
 build(vignettes = FALSE)
 install(quick=FALSE)
-devtools::test()#VerySlow
 check(vignettes  = FALSE)
 vignette(package = "DSIWasteWater")
