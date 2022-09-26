b7ff33af765067f2580f26a2d5e3adbcd3ee997e
Marlin Lee <Marlinrlee0@gmail.com>
2022-09-25 21:52:39 -0500

    get all tests passed

--- tests/testthat/test-Vignette2.R
+++ tests/testthat/test-Vignette2.R
@@ -1,6 +1,5 @@
 test_that("Test vignettes vignettes_DHSTopLevelAnalysis_Outlier produces expected output", {
   data(WasteWater_data, package = "DSIWastewater")
-  DF <- WasteWater_data
   
   baseWaste_data <- buildWasteAnalysisDF(WasteWater_data)
   
@@ -8,38 +7,38 @@ test_that("Test vignettes vignettes_DHSTopLevelAnalysis_Outlier produces expecte
     filter(n >= 180)
   
   workset4_Smooth_data <- baseWaste_data%>%
-    group_by(WWTP)%>%
+    group_by(site)%>%
     group_split()%>%
     lapply(loessSmoothMod)%>%
     bind_rows()
   
-  expect_identical(digest::digest(workset4_Smooth_data),"cffbd30f8cc27d81f4f79f29647770ef")
+  expect_identical(digest::digest(workset4_Smooth_data),"ed472d973d2ce9f75f03073446b0d00c")
   
   df_data <- computeJumps(workset4_Smooth_data)
   
-  expect_identical(digest::digest(df_data),"b754feb98d04336ef52f0f89719b9f26")
+  expect_identical(digest::digest(df_data),"7a454aad75d847afe03599808524f427")
   
   ranked_data <- rankJumps(df_data)
   
-  expect_identical(digest::digest(ranked_data),"e3b23bef3cafe1cec5beb53ccb656c90")
+  expect_identical(digest::digest(ranked_data),"64f6f574f2a623b1bbc28152057b84a6")
   
   ranked_quantile_data <- computeRankQuantiles(ranked_data)
   
-  expect_identical(digest::digest(ranked_quantile_data),"ef916fae88929ecad09159e3a8b7f768")
+  expect_identical(digest::digest(ranked_quantile_data),"caa965e88af6d2169f9b5d335513ef1e")
   
   classied_data <- flagOutliers(ranked_quantile_data, 9)
   
-  expect_identical(digest::digest(classied_data),"3b7616b4ed1ee0a6e812edbe1cbe732c")
+  expect_identical(digest::digest(classied_data),"ccaaaf830d3264ac17be2c601431b1a2")
   
   created_data <- removeOutliers(classied_data)
   
-  expect_identical(digest::digest(created_data),"ab4dc565f5feb7803e491693c8bd0ad6")
+  expect_identical(digest::digest(created_data),"24ae6cb00df5fb4feab0bd0fae54237b")
   
   reg_estimates_data <- buildRegressionEstimateTable(created_data,
                                                      RunOn = c("sars_cov2_adj_load_log10",
                                                                "sars_adj_log10_Filtered",
                                                                "Loess"))
-  expect_identical(digest::digest(reg_estimates_data),"6983cf216761b946aa3671659423b881")
+  expect_identical(digest::digest(reg_estimates_data),"2212ab89ece65876de92fb3b0fcaf661")
   
   
 
